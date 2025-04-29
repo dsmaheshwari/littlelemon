caf45265cdeb578292e6372b121c6b0195d28d6e
DarshanOracle <153791933+DarshanOracle@users.noreply.github.com>
2025-04-29 22:33:24 +0530

    Adding Profile Navigation

--- Android/app/src/main/java/com/example/littlelemon/framework/composable/Header.kt
+++ Android/app/src/main/java/com/example/littlelemon/framework/composable/Header.kt
@@ -2,13 +2,20 @@ package com.example.littlelemon.framework.composable
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.littlelemon.R
 import com.example.littlelemon.ui.theme.CoralRed
 import com.example.navigations.NavigationManager
@@ -18,10 +25,28 @@ fun Header(navigationManager: NavigationManager) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .height(60.dp)
             .background(CoralRed),
-        horizontalArrangement = Arrangement.Center
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Image(painter = painterResource(R.drawable.restaurant_logo), contentDescription = "Restaurant Logo")
+        Image(
+            painter = painterResource(R.drawable.restaurant_logo),
+            contentDescription = "Restaurant Logo",
+            modifier = Modifier
+                .fillMaxSize(0.8F)
+        )
+        Image(
+            imageVector = Icons.Default.Settings,
+            contentDescription = "Settings",
+            modifier = Modifier.clip(
+                RoundedCornerShape(50)
+            )
+            .fillMaxSize(0.5F)
+            .clickable {
+                navigationManager.navigateTo("Profile")
+            }
+        )
     }
 }
 
